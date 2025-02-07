4e26871958cad1e41b672da59f6a111a46c9ec4c
McImp <Magnus_petersen@outlook.com>
2025-02-07 15:27:07 +0100

    ControllerSceneTidsperioder.java og TimePeriodAccess.java fikset

--- src/main/java/com/example/tidsrejseagentur/backend/domain/time_periods/models/TimePeriodRead.java
+++ src/main/java/com/example/tidsrejseagentur/backend/domain/time_periods/models/TimePeriodRead.java
@@ -1,3 +1,8 @@
 package com.example.tidsrejseagentur.backend.domain.time_periods.models;
 
-public record TimePeriodRead(int id, String name, String description) {}
+public record TimePeriodRead(int id, String name, String description) {
+    @Override
+    public String toString() {
+        return name + " - " + description; // Combine name and description for ComboBox display
+    }
+}
