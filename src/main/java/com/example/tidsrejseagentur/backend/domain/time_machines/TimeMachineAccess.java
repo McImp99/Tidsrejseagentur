3db9e20b19449fcce0f043735fb4f23f6e3cec64
McImp <Magnus_petersen@outlook.com>
2025-02-07 14:10:57 +0100

    TimeMachineAccess add function not working

--- src/main/java/com/example/tidsrejseagentur/backend/domain/time_machines/TimeMachineAccess.java
+++ src/main/java/com/example/tidsrejseagentur/backend/domain/time_machines/TimeMachineAccess.java
@@ -67,6 +67,11 @@ public class TimeMachineAccess implements ITimeMachineAccess {
 
     @Override
     public int add(TimeMachineCreate timeMachine) throws SQLException {
+        int capacityValue = timeMachine.capacity();
+        if ("Not in use".equals(timeMachine.capacity())) {
+            capacityValue = 1;
+        }
+
         var stmt = conn.prepareStatement("INSERT INTO time_machines (name, status, capacity) VALUES (?, ?, ?)",
                 Statement.RETURN_GENERATED_KEYS);
         stmt.setString(1, timeMachine.name());
